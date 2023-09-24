a62e14084eba3e87794b756b5b041fc3329bd80c
letvarprint <a.brixencov@icloud.com>
2023-09-24 05:51:37 +0300

    Initial Commit

---
+++ hw2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw2.1
+//
+//  Created by Alexandru Brixencov on 24.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
