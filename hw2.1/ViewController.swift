eba342d8d214ebe0322d91c1e0d8fa9a6d763a93
letvarprint <a.brixencov@icloud.com>
2023-09-24 21:55:19 +0300

    made an app

--- hw2.1/ViewController.swift
+++ hw2.1/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redView.layer.cornerRadius = redView.frame.size.width / 2
+        yellowView.layer.cornerRadius = redView.frame.size.width / 2
+        greenView.layer.cornerRadius = redView.frame.size.width / 2
+        redView.backgroundColor = .gray
+        yellowView.backgroundColor = .gray
+        greenView.backgroundColor = .gray
     }
 
 
+    @IBAction func pressButton() {
+        if redView.backgroundColor == .red {
+            redView.backgroundColor = .gray
+            yellowView.backgroundColor = .yellow
+        } else if yellowView.backgroundColor == .yellow {
+            yellowView.backgroundColor = .gray
+            greenView.backgroundColor = .green
+        } else if greenView.backgroundColor == .green {
+            greenView.backgroundColor = .gray
+            redView.backgroundColor = .red
+        } else {
+            redView.backgroundColor = .red
+            button.setTitle("Next", for: .normal)
+        }
+    }
 }
 
